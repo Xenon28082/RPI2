25b385b02c6b270543e836504391aaf75a704f91
Xenon28082 <savva.roll@gmail.com>
2021-05-30 19:01:14 +0300

    first commit

--- slider.js
+++ slider.js
@@ -31,7 +31,7 @@ function getSlideCount() {
 }
 
 function plusSlides(n) {
-    var slides = document.getElementsByClassName('tryclass');
+    var slides = document.getElementsByClassName('itemclass');
     slideIndex += n;
     showSlides(slideIndex);
     slides[slideIndex - 1].style.backgroundColor = '#' + (Math.random().toString(16) + '000000').substring(2, 8).toUpperCase();
@@ -53,7 +53,7 @@ function initializeSlider(texts) {
         item.style.display = 'flex';
         item.style.justifyContent = "center";
         item.style.alignItems = "center";
-        item.className = 'tryclass';
+        item.className = 'itemclass';
         var parg = document.createElement('p');
         var text = document.createTextNode(texts[i]);
 
@@ -72,7 +72,7 @@ function initializeSlider(texts) {
 }
 
 function currentSlides(n) {
-    var slides = document.getElementsByClassName('tryclass');
+    var slides = document.getElementsByClassName('itemclass');
     showSlides(slideIndex = n);
     slides[slideIndex - 1].style.backgroundColor = '#' + (Math.random().toString(16) + '000000').substring(2,8).toUpperCase();
 }
@@ -80,7 +80,7 @@ function currentSlides(n) {
 function showSlides(n) {
     // alert("pressed: " + n)
     var i;
-    var slides = document.getElementsByClassName('tryclass');
+    var slides = document.getElementsByClassName('itemclass');
     var dots = document.getElementsByClassName('dot');
 
     if (n > slides.length) {
@@ -147,48 +147,4 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 
-});usage: git [--version] [--help] [-C <path>] [-c <name>=<value>]
-           [--exec-path[=<path>]] [--html-path] [--man-path] [--info-path]
-           [-p | --paginate | -P | --no-pager] [--no-replace-objects] [--bare]
-           [--git-dir=<path>] [--work-tree=<path>] [--namespace=<name>]
-           <command> [<args>]
-
-These are common Git commands used in various situations:
-
-start a working area (see also: git help tutorial)
-   clone             Clone a repository into a new directory
-   init              Create an empty Git repository or reinitialize an existing one
-
-work on the current change (see also: git help everyday)
-   add               Add file contents to the index
-   mv                Move or rename a file, a directory, or a symlink
-   restore           Restore working tree files
-   rm                Remove files from the working tree and from the index
-   sparse-checkout   Initialize and modify the sparse-checkout
-
-examine the history and state (see also: git help revisions)
-   bisect            Use binary search to find the commit that introduced a bug
-   diff              Show changes between commits, commit and working tree, etc
-   grep              Print lines matching a pattern
-   log               Show commit logs
-   show              Show various types of objects
-   status            Show the working tree status
-
-grow, mark and tweak your common history
-   branch            List, create, or delete branches
-   commit            Record changes to the repository
-   merge             Join two or more development histories together
-   rebase            Reapply commits on top of another base tip
-   reset             Reset current HEAD to the specified state
-   switch            Switch branches
-   tag               Create, list, delete or verify a tag object signed with GPG
-
-collaborate (see also: git help workflows)
-   fetch             Download objects and refs from another repository
-   pull              Fetch from and integrate with another repository or a local branch
-   push              Update remote refs along with associated objects
-
-'git help -a' and 'git help -g' list available subcommands and some
-concept guides. See 'git help <command>' or 'git help <concept>'
-to read about a specific subcommand or concept.
-See 'git help git' for an overview of the system.
+});
